35f72e4fd3b3d6930618fc0b2b57aab2d2570f35
Jacob Duba <jacob@mooshoe.me>
2024-01-29 17:52:50 -0600

    Path generation

--- duba_jacob.assignment-1.01/generate.c
+++ duba_jacob.assignment-1.01/generate.c
@@ -56,7 +56,6 @@ int main(int argc, char *argv[]) {
   }
 
   struct cord generation_queue[MAP_X_WIDTH * MAP_Y_HEIGHT];
-
   int head, tail = 0;
 
   place_seed(terrain, &head, generation_queue, rand() % MAP_X_WIDTH,
@@ -81,19 +80,21 @@ int main(int argc, char *argv[]) {
       terrain[cur.y][cur.x - 1] = terrain[cur.y][cur.x];
       generation_queue[head++] = (struct cord){cur.x - 1, cur.y};
     }
+
     if (cur.x < MAP_X_WIDTH - 1 && terrain[cur.y][cur.x + 1] == EMPTY) {
       terrain[cur.y][cur.x + 1] = terrain[cur.y][cur.x];
       generation_queue[head++] = (struct cord){cur.x + 1, cur.y};
     }
+
     if (cur.y > 0 && terrain[cur.y - 1][cur.x] == EMPTY) {
       terrain[cur.y - 1][cur.x] = terrain[cur.y][cur.x];
       generation_queue[head++] = (struct cord){cur.x, cur.y - 1};
     }
+
     if (cur.y < MAP_Y_HEIGHT - 1 && terrain[cur.y + 1][cur.x] == EMPTY) {
       terrain[cur.y + 1][cur.x] = terrain[cur.y][cur.x];
       generation_queue[head++] = (struct cord){cur.x, cur.y + 1};
     }
-    // print(terrain);
   }
 
   for (int y = 0; y < MAP_Y_HEIGHT; y++) {
@@ -103,6 +104,61 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+
+  // struct cord west_path_entrance = {0, rand() % (MAP_Y_HEIGHT - 2) + 1};
+  // struct cord east_path_entrance = {MAP_X_WIDTH - 1,
+  //                                   rand() % (MAP_Y_HEIGHT - 2) + 1};
+  // terrain[east_path_entrance.y][east_path_entrance.x] = ROAD;
+
+  int west_path_y = rand() % (MAP_Y_HEIGHT - 2) + 1;
+  int east_path_y = rand() % (MAP_Y_HEIGHT - 2) + 1;
+  int vertical_travel_x = rand() % (MAP_X_WIDTH - 2) + 1;
+
+  int cur_x = 0;
+  int cur_y = west_path_y;
+
+  while (cur_x != vertical_travel_x) {
+    terrain[cur_y][cur_x] = ROAD;
+    cur_x++;
+  }
+
+  while (cur_y != east_path_y) {
+    terrain[cur_y][cur_x] = ROAD;
+    cur_y += (east_path_y - west_path_y) / abs(east_path_y - west_path_y);
+  }
+
+  while (cur_x != MAP_X_WIDTH) {
+    terrain[cur_y][cur_x] = ROAD;
+    cur_x++;
+  }
+
+  int north_path_x = rand() % (MAP_X_WIDTH - 2) + 1;
+  int south_path_x = rand() % (MAP_X_WIDTH - 2) + 1;
+  int horizontal_travel_y = rand() % (MAP_Y_HEIGHT - 2) + 1;
+
+  cur_x = north_path_x;
+  cur_y = 0;
+
+  while (cur_y != horizontal_travel_y) {
+    terrain[cur_y][cur_x] = ROAD;
+    cur_y++;
+  }
+
+  while (cur_x != south_path_x) {
+    terrain[cur_y][cur_x] = ROAD;
+    cur_x += (south_path_x - north_path_x) / abs(south_path_x - north_path_x);
+  }
+
+  while (cur_y != MAP_Y_HEIGHT) {
+    terrain[cur_y][cur_x] = ROAD;
+    cur_y++;
+  }
+  //   struct cord north_path_entrance = {rand() % (MAP_X_WIDTH - 2) + 1, 0;
+  // terrain[north_path_entrance.y][north_path_entrance.x] = ROAD;
+
+  // struct cord south_path_entrance = {rand() % (MAP_X_WIDTH - 2) + 1,
+  //                                    MAP_Y_HEIGHT - 1};
+  // terrain[south_path_entrance.y][south_path_entrance.x] = ROAD;
 
   print(terrain);
 }
